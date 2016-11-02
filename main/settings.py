e26bda5ac53bd125ac0517f6e6de4ef5a5cf71c4
Hyunmin Choi <pointer81@gmail.com>
2016-11-02 11:34:07 +0900

    Added .gitignore

--- main/settings.py
+++ main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -76,9 +77,12 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'db_saveworld',
+        'USER': 'saveworld',
+        'PASSWORD': 'pw-saveworld',
+        'HOST': 'db-saveworld2-dev.cqzylqmcjdma.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '',    }
 }
 
 
