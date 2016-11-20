1a30a67224203a96b14f62c102baacf57583a379
Hyunmin Choi <pointer81@gmail.com>
2016-11-20 13:23:47 +0900

    added inline for item

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from core.models import Item, Image
 
+
+class ImageInline(admin.TabularInline):
+    model = Image
+    extra = 1
+
 class ItemAdmin(admin.ModelAdmin):
+    inlines = (ImageInline, )
     pass
 
 admin.site.register(Item, ItemAdmin)
