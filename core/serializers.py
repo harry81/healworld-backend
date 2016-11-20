649f306f3343b48e0d2c056e757aef25b3eef862
Hyunmin Choi <pointer81@gmail.com>
2016-11-20 13:14:13 +0900

    respect image ids when saving item

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.core.exceptions import ObjectDoesNotExist
 from core.models import Item, Image
 from versatileimagefield.serializers import VersatileImageFieldSerializer
 from rest_framework_gis.serializers import GeoFeatureModelSerializer
@@ -16,14 +17,28 @@ class ImageSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Image
-        fields = ('id', 'item', 'itemshot')
+        fields = ('id', 'itemshot')
 
 
 class ItemSerializer(GeoFeatureModelSerializer):
-    images = ImageSerializer(many=True, read_only=True)
+    image_ids = serializers.CharField(max_length=200, write_only=True)
 
     class Meta:
         model = Item
         geo_field = "point"
 
-        fields = ('memo', 'user', 'created_at', 'images')
+        fields = ('memo', 'user', 'created_at', 'image_ids')
+
+    def create(self, validated_data):
+        image_ids = validated_data.pop('image_ids')
+        item = super(ItemSerializer, self).create(validated_data)
+
+        for image_id in image_ids.split(','):
+            try:
+                image = Image.objects.get(id=image_id)
+                item.images.add(image)
+
+            except ObjectDoesNotExist:
+                pass
+
+        return item
