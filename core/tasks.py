8743274684c705597cdda14e0d84d35d23b58f69
Hyunmin Choi <pointer81@gmail.com>
2017-01-15 16:45:26 +0900

    get ready to use celery

--- core/tasks.py
+++ core/tasks.py
@@ -1,18 +1,19 @@
+# -*- coding: utf-8 -*-
+
 from main.celery_app import app as celery_app
 
-@celery_app.task(bind=True)
-def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
-
 
 @celery_app.task(bind=True)
-def send_email_healworld(self, message):
+def send_email_healworld(self, comment):
     from django.core.mail import send_mail
 
+    item = comment.content_type.get_all_objects_for_this_type().get(
+        id=comment.object_pk)
+
     send_mail(
-        message,
-        'Here is the message.',
+        u'신규 댓글',
+        comment.comment,
         'chharry@gmail.com',
-        ['chharry@gmail.com'],
+        [item.user.email],
         fail_silently=False,
     )
