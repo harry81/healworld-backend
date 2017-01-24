408286b8582d648cd3e243fbbe3b0c26a064bba3
Hyunmin Choi <pointer81@gmail.com>
2017-01-24 16:57:43 +0900

    pep 8

--- core/tests.py
+++ core/tests.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
-from core.models import User, Item
-from django_comments.models import Comment
+from core.models import User
 from .utils import get_short_url
 from django.test import Client
 
@@ -18,14 +17,12 @@ class CoreTests(TestCase):
 
     def test_registration_user(self):
         client = Client()
-        response = client.post('/rest-auth/registration/',
-                               {
-                                   'username': 'john',
-                                   'password1': 'smith1234',
-                                   'password2': 'smith1234',
-                                   'verified_code': '7234'
-                               }
-        )
+        response = client.post('/rest-auth/registration/', {
+            'username': 'john',
+            'password1': 'smith1234',
+            'password2': 'smith1234',
+            'verified_code': '7234'
+        })
         self.assertEqual(response.status_code, 201)
 
         user = User.objects.get(username='john')
@@ -33,37 +30,31 @@ class CoreTests(TestCase):
 
     def test_registration_user_with_wrong_verified_code(self):
         client = Client()
-        response = client.post('/rest-auth/registration/',
-                               {
-                                   'username': 'john',
-                                   'password1': 'smith1234',
-                                   'password2': 'smith1234',
-                                   'verified_code': '1234'
-                               }
-        )
+        response = client.post('/rest-auth/registration/', {
+            'username': 'john',
+            'password1': 'smith1234',
+            'password2': 'smith1234',
+            'verified_code': '1234'
+        })
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(0, User.objects.filter(username='john').count())
 
     def test_login(self):
         client = Client()
-        response = client.post('/rest-auth/registration/',
-                               {
-                                   'username': 'john',
-                                   'password1': 'smith1234',
-                                   'password2': 'smith1234',
-                                   'verified_code': '7234'
-                               }
-        )
+        response = client.post('/rest-auth/registration/', {
+            'username': 'john',
+            'password1': 'smith1234',
+            'password2': 'smith1234',
+            'verified_code': '7234'
+        })
         self.assertEqual(response.status_code, 201)
 
         user = User.objects.get(username='john')
         self.assertIsInstance(user, User)
 
-        response = client.post('/rest-auth/login/',
-                              {
-                                  'username': 'john',
-                                  'password': 'smith1234',
-                              }
-        )
+        response = client.post('/rest-auth/login/', {
+            'username': 'john',
+            'password': 'smith1234',
+        })
         self.assertEqual(response.status_code, 200)
