9a9f6399bf16c342c55548b1e8339c7a937f87c4
Hyunmin Choi <pointer81@gmail.com>
2017-01-26 18:48:42 +0900

    set LANGUAGE_CODE to KO

--- main/settings.py
+++ main/settings.py
@@ -195,7 +195,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-KR'
 
 TIME_ZONE = 'Asia/Seoul'
 
